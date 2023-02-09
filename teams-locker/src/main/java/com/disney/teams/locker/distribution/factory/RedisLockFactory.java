fe66dbb52ef40af2a4e2dcb172cdce251cf31bb2
arron <zhouxianjun@1haitao.com>
2023-02-09 23:03:25 +0800

    add redis factory

---
+++ teams-locker/src/main/java/com/disney/teams/locker/distribution/factory/RedisLockFactory.java
@@ -0,0 +1,17 @@
+package com.disney.teams.locker.distribution.factory;
+
+/**
+ * @author arron.zhou
+ * @version 0.1
+ * @date 2023/2/9
+ * Description:
+ * Modification  History:
+ * Date         Author        Version        Description
+ * ------------------------------------------------------
+ * 2023/2/9      arron.zhou    0.1          create
+ */
+public class RedisLockFactory extends AbstractLockFactory{
+
+
+
+}
